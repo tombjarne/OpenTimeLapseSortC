60ab58ff9f39c80a2f1cfb875e6654c5aa873ec9
tombjarne <bjarne.seidel@icloud.com>
2020-11-26 15:43:31 +0100

    add new models

---
+++ OpenTimelapseSort/Models/DatabaseEntry.cs
@@ -0,0 +1,11 @@
+public class DatabaseEntry
+{
+    public DatabaseEntry()
+    {
+        
+    }
+    // private HashSet<string, string> -> make abstraction in extending classes
+    private string table { get; set; }
+    
+    //converted values? how does that work with entity framework? 
+}
---
+++ OpenTimelapseSort/Models/Import.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OpenTimelapseSort.Models;
+
+namespace OpenTimelapseSort.Models
+{
+    class Import
+    {
+        // only one instance per day ( for Import )
+        // save Import instances with AUTOINCREMENT ID in database 
+
+        public Import()
+        {
+            timestamp = System.DateTime.Today;
+            importDate = timestamp.ToString();
+        }
+
+        public HashSet<Directory> directories;
+        private DateTime timestamp;
+        public String importDate;
+        public int length { get; set; }
+
+        public bool tryPush(Directory directory)
+        {
+            if(directory.getTimestamp() == timestamp)
+            {
+                directories.Add(directory);
+                return true;
+            } else
+            {
+                // false means a new Import Object has to be created as the dates vary
+                return false;
+            }
+        }
+
+    }
+}
